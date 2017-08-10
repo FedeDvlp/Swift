edfd38b0b8408e4154f7b04e2f6bd993a9d075da
Federico <fede_nava@hotmail.com>
2017-08-10 17:49:37 -0500

    Proyecto1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for x in 0...100 {
+    if x % 5 == 0 {
+        print("\(x) bingo!!!")
+    }
+    if x % 2 == 0 {
+        print("\(x) numero par!!!")
+    }
+    if x % 2 == 1 {
+        print("\(x) numero impar!!!")
+    }
+    if x >= 30 && x <= 40 {
+        print("\(x) viva swift!!!")
+    }
+}
+
+//F.V.
